e65d4db26b375e17726230d9945a8710e014898a
Tim Kremer <494033@student.saxion.nl>
2023-05-25 12:01:00 +0200

    added shaddow implementation of observable object

---
+++ Friendsbook/Friendsbook.Core/MVVM/ObservableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Friendsbook.Core.MVVM
+{
+    public class ObservableObject : CommunityToolkit.Mvvm.ComponentModel.ObservableObject
+    {
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            OnPropertyChanging(propertyName);
+        }
+    }
+}
