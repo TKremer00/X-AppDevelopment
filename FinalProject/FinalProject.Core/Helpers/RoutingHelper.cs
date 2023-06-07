6d61a80c06e60e5916b07b814c62db7c94c8d9f0
Tim Kremer <494033@student.saxion.nl>
2023-06-07 18:38:55 +0200

    added finalproject project

---
+++ FinalProject/FinalProject.Core/Helpers/RoutingHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace FinalProject.Core.Helpers
+{
+    public static class RoutingHelper
+    {
+        public static void RegisterRoute<T>() where T : Page
+        {
+            var type = typeof(T);
+            var name = type.Name;
+            Routing.RegisterRoute(name, type);
+        }
+    }
+}
---
+++ FinalProject/FinalProject/MauiProgram.cs
@@ -0,0 +1,47 @@
+﻿using FinalProject.Core.Helpers;
+using FinalProject.Pages;
+using Material.Components.Maui.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace FinalProject;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .RegisterDependencies()
+            .RegisterRoutes()
+            .UseMaterialComponents(
+                new List<string>
+                {
+                    "Roboto-Regular.ttf",
+                    "Roboto-Italic.ttf",
+                    "Roboto-Medium.ttf",
+                    "Roboto-MediumItalic.ttf",
+                    "Roboto-Bold.ttf",
+                    "Roboto-BoldItalic.ttf",
+                }
+            );
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+
+    public static MauiAppBuilder RegisterDependencies(this MauiAppBuilder mauiAppBuilder)
+    {
+        return mauiAppBuilder;
+    }
+
+    public static MauiAppBuilder RegisterRoutes(this MauiAppBuilder mauiAppBuilder)
+    {
+        RoutingHelper.RegisterRoute<MainPage>();
+
+        return mauiAppBuilder;
+    }
+}
