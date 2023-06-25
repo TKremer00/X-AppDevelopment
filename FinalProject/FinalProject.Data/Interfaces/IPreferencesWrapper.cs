d61521cb0dffbd6b153dbeca33f6754b2875b8db
Tim Kremer <494033@student.saxion.nl>
2023-06-25 19:43:46 +0200

    removed T

--- FinalProject/FinalProject.Data/Interfaces/IPreferencesWrapper.cs
+++ FinalProject/FinalProject.Data/Interfaces/IPreferencesWrapper.cs
@@ -4,7 +4,7 @@ namespace FinalProject.Data.Interfaces
 {
     public interface IPreferencesWrapper
     {
-        public event EventHandler<PreferenceUpdate<T>> SettingChanged;
+        public event EventHandler<PreferenceUpdate> SettingChanged;
 
         public T Get<T>(string key, T defaultValue);
 
